ee37fc7a9ce233252f728e8af06feaa1f72f722b
YexiaoyuQing <yq2409@Columbia.edu>
2025-02-19 22:00:46 -0500

    使用 Colab 创建而成

---
+++ WhoAmI_File.py
@@ -0,0 +1,42 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "authorship_tag": "ABX9TyNk3JRjkhFxJTkRClRxrG3P",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/YexiaoyuQing/hw1/blob/main/WhoAmI_File.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "execution_count": null,
+      "metadata": {
+        "id": "NNUyzDYbA9XJ"
+      },
+      "outputs": [],
+      "source": [
+        "def WhoAmI():\n",
+        "    return('yq2409')"
+      ]
+    }
+  ]
+}
